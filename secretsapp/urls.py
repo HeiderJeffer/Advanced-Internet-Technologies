6b8f826d81a32673809cb8fab7d77683b3a69256
Heider Jeffer <hheider.jeffer@gmail.com>
2023-06-05 10:16:16 +0200

    django urls.path change url with path

--- secretsapp/urls.py
+++ secretsapp/urls.py
@@ -1,17 +1,16 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 from secretsmodules import views
-from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # url(r'^secretsmodules/', include('secretsmodules.urls')),
-    #url(r'^admin/', admin.site.urls),
-    url(r'^$', views.HomePageView.as_view(), name = 'index'),
-    url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^login/$', views.LoginForm.as_view(), name='login'),
-    url(r'^', include('secretsmodules.urls')),
+    # path('secretsmodules/', include('secretsmodules.urls')),
+    # path('admin/', admin.site.urls),
+    path('', views.HomePageView.as_view(), name='index'),
+    path('logout/', views.user_logout, name='logout'),
+    path('login/', views.LoginForm.as_view(), name='login'),
+    path('', include('secretsmodules.urls')),
 ]
 #if settings.DEBUG is True:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- secretsmodules/urls.py
+++ secretsmodules/urls.py
@@ -1,17 +1,16 @@
 # howdy/urls.py
 from django.urls import path
-from django.conf.urls import url
+from django.urls import re_path
 from secretsmodules import views
 
 
-app_name = 'secretsmodules'
 urlpatterns = [
-    url(r'^details/(?P<pk>\d{1,50})/$',views.DetailsView.as_view(), name = 'details'),
-    url(r'^add_to_cart/(?P<secret_id>[0-9]+)$', views.add_to_cart, name='add_to_cart'),
-    url(r'^remove_from_cart/(?P<secret_id>[0-9]+)$', views.remove_from_cart, name='remove_from_cart'),
-    url(r'^register/$',views.RegisterForm.as_view(), name='register'),
-    url('cart/', views.get_cart, name='cart'),
-    url('user_secrets/', views.get_user_secrets, name='user_secrets'),
-    url(r'^checkout/$', views.CheckoutView.as_view(), name='checkout'),
-    path('profile/',views.EditProfile.as_view(), name = 'profile'),
+    re_path(r'^details/(?P<pk>\d{1,50})/$', views.DetailsView.as_view(), name='details'),
+    re_path(r'^add_to_cart/(?P<secret_id>[0-9]+)$', views.add_to_cart, name='add_to_cart'),
+    re_path(r'^remove_from_cart/(?P<secret_id>[0-9]+)$', views.remove_from_cart, name='remove_from_cart'),
+    re_path(r'^register/$', views.RegisterForm.as_view(), name='register'),
+    path('cart/', views.get_cart, name='cart'),
+    path('user_secrets/', views.get_user_secrets, name='user_secrets'),
+    re_path(r'^checkout/$', views.CheckoutView.as_view(), name='checkout'),
+    path('profile/', views.EditProfile.as_view(), name='profile'),
 ]
